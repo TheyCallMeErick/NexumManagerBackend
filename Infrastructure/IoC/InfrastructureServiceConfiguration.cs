4f3965b1b3994e3c38ae8d3f666f1e4777f388f7
Azevedo-Erick <azevedoerick762@gmail.com>
2025-11-13 02:11:43 -0300

    [🔧] configure serilog with mongodb sink

--- Infrastructure/IoC/InfrastructureServiceConfiguration.cs
+++ Infrastructure/IoC/InfrastructureServiceConfiguration.cs
@@ -4,12 +4,31 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.MongoDB;
 
-namespace Infrastructure.IoC; 
+namespace Infrastructure.IoC;
+
 public static class InfrastructureServiceConfiguration
 {
     public static void ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        var mongoDbUrl = configuration.GetValue<string>("mongoDbUrl");
+        if (mongoDbUrl == null)
+        {
+            throw new ArgumentException("Cannot connect to mongoDb");
+        }
+        services.AddLogging(builder =>
+        {
+            builder.ClearProviders();
+            builder.AddSerilog();
+        });
+        Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.MongoDBBson(mongoDbUrl, rollingInterval: Serilog.Sinks.MongoDB.RollingInterval.Day)
+                .CreateLogger();
+
         services.AddDbContext<ApplicationDbContext>(options =>
         {
             options.UseSqlite(configuration.GetConnectionString("DefaultConnection"));

bd7d3ff53997cdb293ea62692f1e8b49e443b793
Azevedo-Erick <azevedoerick762@gmail.com>
2025-11-14 08:33:54 -0300

    [✨] add aspire services

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -1,4 +1,29 @@
 
 var builder = DistributedApplication.CreateBuilder(args);
-builder.AddProject<Projects.Api>("api");
+var api = builder.AddProject<Projects.Api>("api");
+
+var postgres = builder.AddPostgres("pgsql", port: 5432)
+    .WithDataVolume()
+    .WithPgAdmin()
+    .AddDatabase("AppDb");
+
+var mongo = builder.AddMongoDB("mongo")
+    .WithDataVolume()
+    .WithMongoExpress();
+
+var rabbit = builder.AddRabbitMQ("rabbitmq", port: 5672);
+
+var redis = builder.AddValkey("cache", port: 6379)
+    .WithDataVolume();
+
+api
+    .WithReference(postgres)
+    .WaitFor(postgres)
+    .WithReference(mongo)
+    .WaitFor(mongo)
+    .WithReference(rabbit)
+    .WaitFor(rabbit)
+    .WithReference(redis)
+    .WaitFor(redis);
+
 builder.Build().Run();

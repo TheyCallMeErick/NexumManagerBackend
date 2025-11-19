a5f98b63a1ef0c3bb2499fe515b9dadadb5152d6
Azevedo-Erick <azevedoerick762@gmail.com>
2025-11-19 04:02:31 -0300

    [✨] add tag relationship

--- Domain/Models/Project.cs
+++ Domain/Models/Project.cs
@@ -5,5 +5,7 @@ public class Project : BaseModel
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public ICollection<UserOnProject> Members { get; set; } = new List<UserOnProject>();
+    public ICollection<Tag> Tags { get; set; } = new List<Tag>();
+
     public ICollection<UserInviteToProject> Invites { get; set; } = new List<UserInviteToProject>();
 }
